a1f7b4b76e85b6f23ab5a4f2d31d1b7ada5ecd8c
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2017-05-29 23:03:03 -0400

    empty examples

--- indexer/src/Text/TumblrPost.hs
+++ indexer/src/Text/TumblrPost.hs
@@ -9,6 +9,8 @@ import Data.Time.Format (defaultTimeLocale, parseTimeOrError)
 import Data.Time.Clock (UTCTime)
 import Data.Aeson (decode)
 import Text.TumblrPost.Internal
+import Text.Pandoc
+import Data.Aeson.AutoType.Alternative (alt)
 
 cliMain :: IO ()
 cliMain = return ()
@@ -26,19 +28,56 @@ parseDate = parseTimeOrError True defaultTimeLocale fmt . T.unpack
   where
     fmt = "%Y-%m-%d %H:%M:%S GMT"
 
+houseStyle :: Pandoc -> Text
+houseStyle = T.pack . writeMarkdown def
+
+parseHTML :: Text -> Text
+parseHTML =
+  houseStyle
+  . handleError
+  . readHtml def
+  . T.unpack
+
+answerContent :: TopLevel -> Text
+answerContent (TopLevel{..}) = parseHTML ans
+  where
+    ans = maybe "" (alt id (const "")) topLevelAnswer
+
+audioContent :: TopLevel -> Text
+audioContent (TopLevel{..}) = undefined
+
+chatContent :: TopLevel -> Text
+chatContent (TopLevel{..}) = undefined
+
+linkContent :: TopLevel -> Text
+linkContent (TopLevel{..}) = undefined
+
+photoContent :: TopLevel -> Text
+photoContent (TopLevel{..}) = undefined
+
+quoteContent :: TopLevel -> Text
+quoteContent (TopLevel{..}) = undefined
+
+textContent :: TopLevel -> Text
+textContent (TopLevel{..}) = undefined
+
+videoContent :: TopLevel -> Text
+videoContent (TopLevel{..}) = undefined
+
 toSimplePost :: TopLevel -> Either String TumblrPost
-toSimplePost (TopLevel{..}) =
+toSimplePost tl@(TopLevel{..}) =
   case topLevelType of
-    "answer" -> Right post
-    "audio" -> Right post
-    "chat" -> Right post
-    "link" -> Right post
-    "photo" -> Right post
-    "quote" -> Right post
-    "text" -> Right post
-    "video" -> Right post
+    "answer" -> withExtractor answerContent
+    "audio" -> withExtractor audioContent
+    "chat" -> withExtractor chatContent
+    "link" -> withExtractor linkContent
+    "photo" -> withExtractor photoContent
+    "quote" -> withExtractor quoteContent
+    "text" -> withExtractor textContent
+    "video" -> withExtractor videoContent
     _ -> Left "unknown type"
   where
+    withExtractor f = Right (post { _content = f tl })
     post = TumblrPost { _postUrl = topLevelPostUrl
                       , _date = parseDate topLevelDate
                       , _content = ""
--- indexer/test/Text/TumblrPostSpec.hs
+++ indexer/test/Text/TumblrPostSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Text.TumblrPostSpec (main, spec) where
 
@@ -10,6 +11,7 @@ import Data.Time.Clock (UTCTime(..), secondsToDiffTime)
 import Data.Aeson (decode)
 import Data.Maybe (fromJust)
 import qualified Data.ByteString.Lazy as BL
+import Data.String.Here.Uninterpolated (hereFile)
 
 answer :: TopLevel
 answer = fromJust (decode answerJSON)
@@ -43,28 +45,52 @@ fromUTC year month day hours minutes seconds =
     seconds' = 3600 * hours + 60 * minutes + seconds
 
 answerPost :: TumblrPost
-answerPost = TumblrPost {_postUrl = "http://polychora.tumblr.com/post/100643515987/i-think-game-strong-is-aave-so-dont", _content = "", _date = fromUTC 2014 10 22 04 11 30, _id = 100643515987}
+answerPost = TumblrPost {_postUrl = "http://polychora.tumblr.com/post/100643515987/i-think-game-strong-is-aave-so-dont"
+                        , _content = [hereFile|test/examples/texts/answer.txt|]
+                        , _date = fromUTC 2014 10 22 04 11 30
+                        , _id = 100643515987}
 
 audioPost :: TumblrPost
-audioPost = TumblrPost {_postUrl = "http://polychora.tumblr.com/post/136678974792/disneygraded-boadicea-by-enya", _content = "", _date = fromUTC 2016 01 05 14 42 29, _id = 136678974792}
+audioPost = TumblrPost { _postUrl = "http://polychora.tumblr.com/post/136678974792/disneygraded-boadicea-by-enya"
+                       , _content = [hereFile|test/examples/texts/audio.txt|]
+                       , _date = fromUTC 2016 01 05 14 42 29
+                       , _id = 136678974792}
 
 chatPost :: TumblrPost
-chatPost = TumblrPost {_postUrl = "http://polychora.tumblr.com/post/132627508302/friend-shows-me-a-bad-picture-they-took-of", _content = "", _date = fromUTC 2015 11 05 23 06 05, _id = 132627508302}
+chatPost = TumblrPost {_postUrl = "http://polychora.tumblr.com/post/132627508302/friend-shows-me-a-bad-picture-they-took-of"
+                      , _content = [hereFile|test/examples/texts/chat.txt|]
+                      , _date = fromUTC 2015 11 05 23 06 05
+                      , _id = 132627508302}
 
 linkPost :: TumblrPost
-linkPost = TumblrPost {_postUrl = "http://polychora.tumblr.com/post/100522740692/jobhaver-angelboyangelboy-facebook-deleted", _content = "", _date = fromUTC 2014 10 20 20 04 06, _id = 100522740692}
+linkPost = TumblrPost {_postUrl = "http://polychora.tumblr.com/post/100522740692/jobhaver-angelboyangelboy-facebook-deleted"
+                      , _content = [hereFile|test/examples/texts/link.txt|]
+                      , _date = fromUTC 2014 10 20 20 04 06
+                      , _id = 100522740692}
 
 photoPost :: TumblrPost
-photoPost = TumblrPost {_postUrl = "http://polychora.tumblr.com/post/100083735852/briannamccarthy-a-song-to-my-earth-and-sky", _content = "", _date = fromUTC 2014 10 15 16 08 58, _id = 100083735852}
+photoPost = TumblrPost {_postUrl = "http://polychora.tumblr.com/post/100083735852/briannamccarthy-a-song-to-my-earth-and-sky"
+                       , _content = [hereFile|test/examples/texts/photo.txt|]
+                       , _date = fromUTC 2014 10 15 16 08 58
+                       , _id = 100083735852}
 
 quotePost :: TumblrPost
-quotePost = TumblrPost {_postUrl = "http://polychora.tumblr.com/post/101094725562/welcoming-people-into-the-trans-community-who", _content = "", _date = fromUTC 2014 10 27 16 28 07, _id = 101094725562}
+quotePost = TumblrPost {_postUrl = "http://polychora.tumblr.com/post/101094725562/welcoming-people-into-the-trans-community-who"
+                       , _content = [hereFile|test/examples/texts/quote.txt|]
+                       , _date = fromUTC 2014 10 27 16 28 07
+                       , _id = 101094725562}
 
 textPost :: TumblrPost
-textPost = TumblrPost {_postUrl = "http://polychora.tumblr.com/post/101094758347/infinitybiscuit-tbh-im-still-not-a-fan-of-even", _content = "", _date = fromUTC 2014 10 27 16 28 40, _id = 101094758347}
+textPost = TumblrPost {_postUrl = "http://polychora.tumblr.com/post/101094758347/infinitybiscuit-tbh-im-still-not-a-fan-of-even"
+                      , _content = [hereFile|test/examples/texts/text.txt|]
+                      , _date = fromUTC 2014 10 27 16 28 40
+                      , _id = 101094758347}
 
 videoPost :: TumblrPost
-videoPost = TumblrPost {_postUrl = "http://polychora.tumblr.com/post/102611761812/blckgrlothrwrld-the-watermelon-woman-1996", _content = "", _date = fromUTC 2014 11 14 14 53 20, _id = 102611761812}
+videoPost = TumblrPost {_postUrl = "http://polychora.tumblr.com/post/102611761812/blckgrlothrwrld-the-watermelon-woman-1996"
+                       , _content = [hereFile|test/examples/texts/video.txt|]
+                       , _date = fromUTC 2014 11 14 14 53 20
+                       , _id = 102611761812}
 
 main :: IO ()
 main = hspec spec
